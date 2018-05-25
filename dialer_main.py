4b531b36ae04d217c8f999f908a1731bdf630e5f
Vishal Gupta <vishalg8897@gmail.com>
2018-05-25 09:25:50 +0530

    Bug fixes. Removed dialer_mid

--- dialer_main.py
+++ dialer_main.py
@@ -4,22 +4,13 @@ from functools import partial
 from design2 import Ui_Dialog, _translate
 import argparse
 
-# This file holds our MainWindow and all design related things
-# it also keeps events etc that we defined in Qt Designer
 
-
-class ExampleApp(QtGui.QDialog, Ui_Dialog):
+class DialerApp(QtGui.QDialog, Ui_Dialog):
     def __init__(self, num):
-        # Explaining super is out of the scope of this article
-        # So please google it if you're not familar with it
-        # Simple reason why we use it here is that it allows us to
-        # access variables, methods etc in the design.py file
         super(self.__class__, self).__init__()
         self.setupUi(self)
         self.objectMapSetup()
 
-        # This is defined in design.py file automatically
-        # It sets up layout and widgets that are defined
         if num is not None:
             self.setDialerNumber(num)
 
@@ -51,16 +42,16 @@ class ExampleApp(QtGui.QDialog, Ui_Dialog):
 
 def main(num):
     app = QtGui.QApplication(sys.argv)  # A new instance of QApplication
-    # We set the form to be our ExampleApp (design)
-    form = ExampleApp(num)
+    # We set the form to be our DialerApp (design)
+    form = DialerApp(num)
     form.show()                         # Show the form
     app.exec_()                         # and execute the app
 
 
-if __name__ == '__main__':              # if we're running file directly and not importing it
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Arguments for calling dialer_main')
     parser.add_argument("-n", "--num", help="Number", type=str, default=None)
     args = parser.parse_args()
     number = args.num
-    main(number)                              # run the main function
+    main(number)
