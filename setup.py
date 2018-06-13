1c63f68dced7c3160358de3d5e5ad131fbe5baf3
Vishal Gupta <vishalg8897@gmail.com>
2018-06-14 01:32:39 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='debdialer',
+      version='0.1',
+      description='Click-to-dial pop-up window.',
+      url='https://salsa.debian.org/comfortablydumb-guest/Hello-from-the-Debian-side',
+      author='Vishal Gupta',
+      author_email='vishalg8897@gmail.com',
+      license='GNU',
+      packages=['debdialer'],
+      zip_safe=False)
