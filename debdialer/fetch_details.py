f91599aa8921fcefa8865759f0843104f12a3fb1
Vishal Gupta <vishalg8897@gmail.com>
2018-07-28 13:16:51 +0530

    Added documentation to fetch_details.py

--- debdialer/fetch_details.py
+++ debdialer/fetch_details.py
@@ -1,19 +1,16 @@
 from phonenumbers import timezone, carrier, format_number, PhoneNumberFormat, PhoneNumberMatcher
+from pkg_resources import resource_filename
 from json import load
 
-import phonenumbers
-from pkg_resources import resource_filename
-
-x = phonenumbers.parse("9176119388", "IN")
-
+# Json file mapping dialer codes to countries/
 SOURCE_FILE = 'resources/DialerCodes.json'
 SOURCE_FILE_PATH = resource_filename(__name__, SOURCE_FILE)
-
 with open(SOURCE_FILE_PATH) as f:
     CC_dict = load(f)
 
 
 def get_carrier(x):
+    """Returns carrier corresponding to given phone number."""
     return carrier.name_for_number(x, "en")
 
 
@@ -22,6 +19,13 @@ def get_timezone(x):
 
 
 def formatNum(x, kind=None):
+    """Accepts PhoneNumber object and returns formatted number as a string.
+    Args:
+        kind :  If None, returns number in E164 format'
+                else
+                    If 'national', return number in NATIONAL format
+                    else, return number in INTERNATIONAL format
+    """
     if kind:
         if kind == 'national':
             return format_number(x, PhoneNumberFormat.NATIONAL)
@@ -32,23 +36,15 @@ def formatNum(x, kind=None):
 
 
 def get_country(x):
+    """Returns country details corresponding to given dialer code (x)."""
     return CC_dict[str(x)]
 
-def parse_file_for_nums(fpath,country_code):
-    with open(fpath,'r') as f:
+
+def parse_file_for_nums(fpath, country_code):
+    """Accepts a filepath & country_code.
+    Returns all valid phonenumbers present in file."""
+    with open(fpath, 'r') as f:
         text = f.read()
-    print ("Parsing",fpath,"for","numbers. Code :",country_code)
+    print("Parsing", fpath, "for", "numbers. Code :", country_code)
     matches = PhoneNumberMatcher(text, country_code)
     return list([formatNum(x.number) for x in matches])
-
-"""
-formatter = phonenumbers.AsYouTypeFormatter("IN")
-formatter.clear()
-formatter.input_digit(u'9176119388')
-
-
-in_number = phonenumbers.parse("9176119388", "IN")
-in_number
-from phonenumbers import geocoder
-geocoder.description_for_number(in_number, "IN")
-"""
