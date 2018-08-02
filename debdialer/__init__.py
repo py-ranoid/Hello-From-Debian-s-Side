588d722156264a0a07e84cadd4d9f692ff685eb6
Vishal Gupta <vishalg8897@gmail.com>
2018-08-02 12:34:09 +0530

    Added no-gui arg to __init__.py

--- debdialer/__init__.py
+++ debdialer/__init__.py
@@ -1,4 +1,3 @@
-from .dialer_main import main
 import argparse
 
 def cli_main():
@@ -7,14 +6,21 @@ def cli_main():
         description='Arguments for calling dialer_main')
     parser.add_argument("-n", "--num", dest='num', help="Open Debdialer with phonenumber", type=str, default='9988776655',nargs='?')
     parser.add_argument("-u", "--url", dest='url',help="Open Debdialer with URL", type=str, default='tel:9988776655',nargs='?')
+    parser.add_argument("-ng", "--no-gui", dest='nogui',help="Open Debdialer with dmenu", action='store_true',default=False)
     args = parser.parse_args()
+
     if args.url:
         url_entered = args.url.strip()
         if url_entered.startswith('tel:'):
             number = url_entered.split(':')[1]
     else:
         number = args.num
-    try:
-        main(number)
-    except KeyboardInterrupt:
-        print ("Interrupt")
+
+    if (args.nogui):
+        print ("NOGUI")
+    else:
+        from .dialer_main import main
+        try:
+            main(number)
+        except KeyboardInterrupt:
+            print ("Interrupt")
