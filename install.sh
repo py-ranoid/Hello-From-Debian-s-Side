6ab2ebc7023f9b93498780f0064e42879806ade3
Vishal Gupta <vishalg8897@gmail.com>
2018-08-04 00:38:42 +0530

    Bug fixes

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo pip3 install debdialer
 SALSA_URL=https://salsa.debian.org/comfortablydumb-guest/Hello-from-the-Debian-side/raw/master
 sudo wget $SALSA_URL/Images/deblogo-128.png -O /usr/share/icons/hicolor/128x128/apps/deblogo-128.png -nv
-sudo wget $SALSA_URL/debdialer.desktop -O /usr/share/applications/debdialer.desktop-nv
+sudo wget $SALSA_URL/debdialer.desktop -O /usr/share/applications/debdialer.desktop -nv
 sudo wget $SALSA_URL/debdialer.conf -O /etc/debdialer.conf -nv
 sudo update-desktop-database /usr/share/applications/
